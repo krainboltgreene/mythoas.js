8449adb380d31822bc663d7e0a6c57d0303cfd59
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2015-12-23 04:06:59 -0800

    Turns out env variables are hard

--- source/index.js
+++ source/index.js
@@ -1,9 +1,18 @@
 import http from "http"
 import connect from "connect"
+import requireEnvironmentVariables from "require-environment-variables"
 import server from "./server"
 
+requireEnvironmentVariables([
+  "KEENIO_API_PUBLIC",
+  "KEENIO_API_PRIVATE",
+  "PUSHER_API_ID",
+  "PUSHER_API_PUBLIC",
+  "PUSHER_API_PRIVATE",
+  "PORT"
+])
+
 const Application = connect()
-const PORT = 3000
 
 function close ({response, environment}) {
 
@@ -17,4 +26,4 @@ function close ({response, environment}) {
 
 Application.use((request, response) => close(server(request, response)))
 
-http.createServer(Application).listen(process.env.PORT || PORT)
+http.createServer(Application).listen(prop("PORT", process.env))
