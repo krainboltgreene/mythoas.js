f1eef8dc59f61cca550331a6a08a6a7651ed6b07
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2015-12-23 04:09:45 -0800

    Setting up pusher/keen credentials

--- source/server/initialState/index.js
+++ source/server/initialState/index.js
@@ -1,4 +1,17 @@
-import winston from "winston"
+import {toPairs, all, prop} from "ramda"
+import Keen from "keen-js"
+import Pusher from "pusher"
+
+const keen = new Keen({
+  "projectId": prop("KEENIO_API_PUBLIC", process.env),
+  "writeKey": prop("KEENIO_API_PRIVATE", process.env)
+})
+const pusher = new Pusher({
+  "appId": prop("PUSHER_API_ID", process.env),
+  "key": prop("PUSHER_API_PUBLIC", process.env),
+  "secret": prop("PUSHER_API_PRIVATE", process.env),
+  "encrypted": true
+})
 
 function dispatch ({request, pattern}) {
 
