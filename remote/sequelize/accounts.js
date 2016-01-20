d2fa862bd6c23ced362a2662339213078c39e7d4
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-01-19 16:06:11 -0800

    I like UUIds better

--- remote/sequelize/accounts.js
+++ remote/sequelize/accounts.js
@@ -1,17 +1,29 @@
 import Sequelize from "sequelize"
 
 const table = "accounts"
+const id = {
+  type: Sequelize.UUID,
+  primaryKey: true
+}
 const email = {
-  type: Sequelize.STRING
+  type: Sequelize.STRING,
+  allowNull: false,
+  unique: true
 }
 const name = {
-  type: Sequelize.STRING
+  type: Sequelize.STRING,
+  allowNull: false,
+  defaultValue: ""
 }
 const attributes = {
+  id,
   email,
   name
 }
-const configuration = {}
+const configuration = {
+  timestamps: true,
+  paranoid: true
+}
 
 export default {
   table,
